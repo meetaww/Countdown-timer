df95ebec659c7d8203ceb7abf5968240467c4d9b
meetaww <86891504+meetaww@users.noreply.github.com>
2022-02-27 18:47:36 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+const daysE1 = document.getElementById('days');
+const hoursE1 = document.getElementById('hours');
+const minsE1 = document.getElementById('mins');
+const secsE1 = document.getElementById('secs');
+
+const newYears = '1 Jan 2023';
+
+function countdown(){
+    const newYearDate = new Date(newYears)
+    const currentDate  = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) /1000;
+
+    const days = Math.floor(totalSeconds / 3600 /24);
+    const hours = Math.floor(totalSeconds / 3600 ) %24;
+    const mins = Math.floor(totalSeconds / 60) %60;
+    const secs = Math.floor(totalSeconds ) %60;
+
+    console.log(days, hours, mins, secs)
+    
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = hours;
+    minsE1.innerHTML = mins;
+    secsE1.innerHTML = secs;
+
+}
+
+function formatTime(time){
+    return time < 10 ? (`0$(time)`) : time;
+}
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000)
+
+
